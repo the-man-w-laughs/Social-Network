63515e3e76fede67694c2be13ea0a85e996a5c41
Nazar <nazarmasukov@gmail.com>
2023-06-20 21:41:01 +0300

    Fix base-repository.

--- SocialNetwork/SocialNetwork.DAL/Contracts/Base/IRepository.cs
+++ SocialNetwork/SocialNetwork.DAL/Contracts/Base/IRepository.cs
@@ -1,11 +1,12 @@
+using Microsoft.AspNetCore.JsonPatch;
 using System.Linq.Expressions;
 
 namespace SocialNetwork.DAL.Contracts.Base;
 
 public interface IRepository<TEntity> where TEntity : class, new()
 {
-    Task<List<TEntity>> Select(Expression<Func<TEntity, bool>>? whereFilter = null);
+    Task<List<TEntity?>> Select(Expression<Func<TEntity, bool>>? whereFilter = null);
     Task<TEntity> Add(TEntity entity);
-    Task<TEntity> Update(TEntity entity);
-    Task<TEntity> Delete(TEntity entity);
+    Task<TEntity?> Update(uint id, JsonPatchDocument entity);
+    Task<TEntity?> Delete(uint id);
 }
--- SocialNetwork/SocialNetwork.DAL/Repositories/Base/Repository.cs
+++ SocialNetwork/SocialNetwork.DAL/Repositories/Base/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.EntityFrameworkCore;
 using SocialNetwork.DAL.Context;
 using SocialNetwork.DAL.Contracts.Base;
@@ -11,12 +12,18 @@ public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity :
 
     protected Repository(SocialNetworkContext socialNetworkContext) => SocialNetworkContext = socialNetworkContext;
 
-    public virtual async Task<List<TEntity>> Select(Expression<Func<TEntity, bool>>? whereFilter = null)
+    public virtual async Task<List<TEntity?>> Select(Expression<Func<TEntity, bool>>? whereFilter = null)
     {
-        var resultSet = SocialNetworkContext.Set<TEntity>();
-        return await (whereFilter == null 
-            ? resultSet.ToListAsync() 
-            : resultSet.Where(whereFilter).ToListAsync());
+        DbSet<TEntity> resultSet = SocialNetworkContext.Set<TEntity>();
+
+        if (whereFilter == null)
+        {
+            return await resultSet.ToListAsync();
+        }
+        else
+        {
+            return await resultSet.Where(whereFilter).ToListAsync();
+        }
     }
 
     public virtual async Task<TEntity> Add(TEntity entity)
@@ -26,17 +33,31 @@ public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity :
         return entity;
     }
 
-    public virtual async Task<TEntity> Update(TEntity entity)
+    public async Task<TEntity?> Update(uint id, JsonPatchDocument entity)
     {
-        var entityEntry = SocialNetworkContext.Update(entity);
+        var entityQuery = await SocialNetworkContext.FindAsync<TEntity>(id);
+        if (entityQuery == null)
+        {
+            return entityQuery;
+        }
+        entity.ApplyTo(entityQuery);
         await SocialNetworkContext.SaveChangesAsync();
-        return entityEntry.Entity;
+
+        return entityQuery;
     }
 
-    public virtual async Task<TEntity> Delete(TEntity entity)
+    public virtual async Task<TEntity?> Delete(uint id)
     {
-        var entityEntry = SocialNetworkContext.Remove(entity);
-        await SocialNetworkContext.SaveChangesAsync();
-        return entityEntry.Entity;
+        var entity = await SocialNetworkContext.FindAsync<TEntity>(id);
+        if (entity != null)
+        {
+            var entityEntry = SocialNetworkContext.Remove(entity);
+            await SocialNetworkContext.SaveChangesAsync();
+            return entityEntry.Entity;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
