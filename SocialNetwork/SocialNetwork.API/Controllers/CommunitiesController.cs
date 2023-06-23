5af48cfc5aeb5247de6acef16f9a5f87e1ba91f2
Nazar <nazarmasukov@gmail.com>
2023-06-23 11:57:55 +0300

    Document community controller.

--- SocialNetwork/SocialNetwork.API/Controllers/CommunitiesController.cs
+++ SocialNetwork/SocialNetwork.API/Controllers/CommunitiesController.cs
@@ -45,11 +45,14 @@ public class CommunitiesController : ControllerBase
     }
 
     /// <summary>
-    /// CreateCommunity
+    /// CreateCommunity.
     /// </summary>
-    /// <remarks>Create community.</remarks>
+    /// <remarks>Create a community.</remarks>
+    /// <param name="communityRequestDto">The information of the community to create.</param>    
+    /// <response code="200">Returns the created community.</response>
     [HttpPost]
     [Authorize(Roles = "Admin, User")]
+    [ProducesResponseType(typeof(CommunityResponseDto), StatusCodes.Status200OK)]
     public virtual async Task<ActionResult<CommunityResponseDto>> PostCommunities(
         [FromBody, Required] CommunityRequestDto communityRequestDto)
     {
@@ -84,9 +87,13 @@ public class CommunitiesController : ControllerBase
     /// <summary>
     /// GetAllCommunities
     /// </summary>
-    /// <remarks>Get all communities using pagination.</remarks>    
-    [HttpGet]      
+    /// <remarks>Get all communities using pagination.</remarks>
+    /// <param name="limit">The maximum number of communities to retrieve.</param>
+    /// <param name="currCursor">The current cursor position for pagination.</param>    
+    /// <response code="200">Returns the list of communities.</response>
+    [HttpGet]
     [Authorize(Roles = "Admin, User")]
+    [ProducesResponseType(typeof(List<CommunityResponseDto>), StatusCodes.Status200OK)]
     public virtual async Task<ActionResult<List<CommunityResponseDto>>> GetCommunities(
         [FromQuery, Required] int limit,
         [FromQuery, Required] int currCursor)
@@ -100,9 +107,17 @@ public class CommunitiesController : ControllerBase
     /// ChangeCommunityInfo
     /// </summary>
     /// <remarks>Change community info (for community admins, admins).</remarks>        
+    /// <param name="communityId">The ID of the community to update.</param>
+    /// <param name="communityPatchRequestDto">The data for updating the community.</param>    
+    /// <response code="200">Returns the updated community.</response>
+    /// <response code="400">If the community with the specified ID doesn't exist or an error occurs during the update.</response>
+    /// <response code="403">If the user is not authorized to update the community.</response>
     [HttpPatch]
     [Authorize(Roles = "Admin, User")]
     [Route("{communityId}")]
+    [ProducesResponseType(typeof(CommunityResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public async virtual Task<ActionResult<CommunityResponseDto>> PatchCommunitiesCommunityId(
         [FromRoute, Required] uint communityId,
         [FromBody, Required] CommunityPatchRequestDto communityPatchRequestDto)
@@ -150,9 +165,16 @@ public class CommunitiesController : ControllerBase
     /// DeleteCommunity
     /// </summary>
     /// <remarks>Delete community (for community admins, admins).</remarks>          
+    /// <param name="communityId">The ID of the community to delete.</param>   
+    /// <response code="200">Returns the deleted community.</response>
+    /// <response code="400">If the community with the specified ID doesn't exist.</response>
+    /// <response code="403">If the user is not authorized to delete the community.</response>
     [HttpDelete]
     [Authorize(Roles = "Admin, User")]
     [Route("{communityId}")]
+    [ProducesResponseType(typeof(CommunityResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public virtual async Task<ActionResult<CommunityResponseDto>> DeleteCommunitiesCommunityId([FromRoute, Required] uint communityId)
     {
         var community = await _communityService.GetCommunityById(communityId);
@@ -191,10 +213,18 @@ public class CommunitiesController : ControllerBase
     /// <summary>
     /// CreateCommunityPost
     /// </summary>
-    /// <remarks>Create community post (depending on isPrivate field it could be available for community members or for everybody).</remarks>         
+    /// <remarks>Create community post (depending on the isPrivate field, it could be available for community members or for everybody).</remarks>         
+    /// <param name="communityId">The ID of the community where the post will be created.</param>
+    /// <param name="communityRequestDto">The post data.</param>   
+    /// <response code="200">Returns the created post.</response>
+    /// <response code="400">If the community with the specified ID doesn't exist.</response>
+    /// <response code="403">If the user is not authorized to create a post in the community.</response>
     [HttpPost]
     [Authorize(Roles = "Admin, User")]
     [Route("{communityId}/posts")]
+    [ProducesResponseType(typeof(PostResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public virtual async Task<ActionResult<PostResponseDto>> PostCommunitiesCommunityIdPosts(
         [FromRoute, Required] uint communityId,
         [FromBody, Required] PostRequestDto communityRequestDto)
@@ -244,9 +274,16 @@ public class CommunitiesController : ControllerBase
     /// GetAllCommunityPosts
     /// </summary>
     /// <remarks>Get all community posts using pagination.</remarks>    
+    /// <param name="communityId">The ID of the community.</param>
+    /// <param name="limit">The maximum number of posts to retrieve.</param>
+    /// <param name="currCursor">The cursor for pagination.</param>    
+    /// <response code="200">Returns the list of community posts.</response>
+    /// <response code="400">If the community with the specified ID doesn't exist.</response>
     [HttpGet]
     [Authorize(Roles = "Admin, User")]
     [Route("{communityId}/posts")]
+    [ProducesResponseType(typeof(List<CommunityPostResponseDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public virtual async Task<ActionResult<List<CommunityPostResponseDto>>> GetCommunitiesPosts(
         [FromRoute, Required] uint communityId,
         [FromQuery, Required] int limit,
@@ -261,8 +298,14 @@ public class CommunitiesController : ControllerBase
     /// CreateCommunityMedia
     /// </summary>
     /// <remarks>Create community media.</remarks>    
+    /// <param name="communityId">The ID of the community.</param>
+    /// <param name="files">The media files to upload.</param>    
+    /// <response code="200">Indicates that the upload was successful.</response>
+    /// <response code="400">If no files are provided.</response>
     [HttpPost]
     [Route("{communityId}/medias")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public virtual ActionResult<List<MediaResponseDto>> PostcommunityscommunityIdMedias([FromRoute][Required] uint communityId, [Required] List<IFormFile> files)
     {
         if (files == null)
@@ -290,12 +333,17 @@ public class CommunitiesController : ControllerBase
     }
 
     /// <summary>
-    /// GetAllcommunityMedias
+    /// GetAllCommunityMedias
     /// </summary>
-    /// <remarks>Get all community's posts using pagination. (only for community or admin)</remarks>
+    /// <remarks>Get all community's media using pagination. (only for community or admin)</remarks>
+    /// <param name="communityId">The ID of the community.</param>
+    /// <param name="limit">The maximum number of media items to retrieve.</param>
+    /// <param name="currCursor">The cursor for pagination.</param>   
+    /// <response code="200">Returns the list of community media.</response>
     [HttpGet]
     [Route("{communityId}/medias")]
-    public async virtual Task<ActionResult<List<CommunityMediaOwnerResponseDto>>> GetcommunityscommunityIdMedias(
+    [ProducesResponseType(typeof(List<MediaResponseDto>), StatusCodes.Status200OK)]
+    public async virtual Task<ActionResult<List<MediaResponseDto>>> GetcommunityscommunityIdMedias(
         [FromRoute, Required] uint communityId,
         [FromQuery, Required] int limit,
         [FromQuery] int currCursor)
